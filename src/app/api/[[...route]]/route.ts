0d6617905b36dee34d49526374e4346ae3dc2efc
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-02-27 01:26:27 +0300

    fix: add health route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,8 +2,6 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import app from "@/server";
 
-export const runtime = "edge";
-
 export const GET = handle(app);
 export const POST = handle(app);
 export const PUT = handle(app);
