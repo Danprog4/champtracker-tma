21fa97d70284bd322ca504e83b51d8867651b2f8
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-03-05 15:23:33 +0300

    test

--- src/components/ChallengesPage/Slider/SliderView.tsx
+++ src/components/ChallengesPage/Slider/SliderView.tsx
@@ -29,45 +29,29 @@ const DumpSlider: React.FC<DumpSliderProps> = ({
   onScroll,
   sliderRefs,
 }) => (
-  <div className="flex flex-col">
+  <div className="flex flex-col w-screen h-screen overflow-x-hidden overflow-y-auto">
     {categories.map((category, categoryIndex) => (
       <div key={categoryIndex} className="mb-8 pl-3">
         <div className="flex justify-between pr-3 items-start mb-3">
           <div className="">{category.title}</div>
-          <div className="text-neutral-400 text-sm mt-1">
-            {currentSlide[categoryIndex] || 1}/{category.items.length}
-          </div>
+          <div className="text-neutral-400 text-sm mt-1"></div>
         </div>
         <div
-          ref={(el) => {
-            if (el) {
-              sliderRefs.current[categoryIndex] = el;
-            }
-          }}
-          onScroll={() => onScroll(categoryIndex)}
-          className={`flex space-x-4    gap-2 pb-4 w-full `}>
+          className={`flex snap-x snap-mandatory space-x-4 overflow-auto overflow-y-hidden scroll-smooth gap-2 pb-4 w-screen `}>
           {category.items.map((card, cardIndex) => (
-            <Link
-              onClick={() => {
-                updateOnBoarding(true);
-              }}
-              to="/card/$id"
-              params={{ id: String(card.id) }}
+            <div
               key={cardIndex}
-              className={`relative flex-shrink-0 bg-cover ${category.color} h-[250px] w-[250px]`}>
+              className={`relative flex-shrink-0 bg-cover ${category.color} h-[250px] w-[250px]`}
+              style={{ transform: "t" }}>
               <div className="z-50 text-outline font-druk absolute inset-x-0 top-0 p-3 text-start text-lg leading-7 text-black">
                 {card.title}
               </div>
-              <Image
+              <img
                 src={card.imageUrl}
                 alt={card.title}
                 className="absolute bottom-0 right-0 w-[180px] h-[180px] object-cover rounded-lg"
-                loading="eager"
-                priority={cardIndex < 4}
-                width={180}
-                height={180}
               />
-            </Link>
+            </div>
           ))}
         </div>
       </div>
