5fc4ae4c772578cef05908a27ff4a2101b5d373d
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-03-07 22:57:20 +0300

    Add conditional carousel display based on user onboarding status

--- src/tanstack-pages/InitiallPage/InitiallPage.tsx
+++ src/tanstack-pages/InitiallPage/InitiallPage.tsx
@@ -1,9 +1,15 @@
 import EmptyState from "@/components/InitiallPage/EmpyState";
 import Header from "@/components/InitiallPage/Header";
+import { CarouselDApiDemo } from "../CarouselPage/CarouselPage";
+import { useUser } from "@/hooks/useUser";
 
 const InitiallPage = () => {
+  const { user } = useUser();
+  const isOnboarding = user.onBoarding;
+  console.log(isOnboarding === false, "trueornot");
   return (
     <div className="flex h-screen flex-col">
+      {isOnboarding === false && <CarouselDApiDemo />}
       <Header />
       <EmptyState />
     </div>
