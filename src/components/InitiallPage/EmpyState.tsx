0751531edc551ee8a3b4dfc61540876b44cc9f12
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-03-07 23:21:28 +0300

    Center empty state button using absolute positioning and transforms

--- src/components/InitiallPage/EmpyState.tsx
+++ src/components/InitiallPage/EmpyState.tsx
@@ -4,7 +4,7 @@ const EmptyState = () => {
   return (
     <div className=" flex h-full flex-col items-center justify-between">
       <Link to="/new">
-        <div className="relative mt-40 mb-3 flex aspect-square w-[100vw] items-center justify-center rounded-full bg-yellow-400">
+        <div className="absolute left-1/2 top-1/2 mb-3 flex aspect-square w-[100vw] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-yellow-400">
           <div className=" text-center">
             <div className=" text-2xl font-druk text-black">
               СОЗДАТЬ НОВОЕ <br />
